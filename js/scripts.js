1ba4d6c2fa0abfd9a52b846abbab89b12b328654
jdc <campajake@gmail.com>
2017-01-17 08:37:35 -0800

    add startup files, create basis javascript functions, add inputs for numbers

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(function() {
+  var InputNumberOne = NAN;
+  var InputNumberTwo = NAN;
+  var onlyDigitRegEx = /^\d+$/;
+  var numberCheck = function(n1) {
+    if (onlyDigitRegEx(n1)) {
+      return true
+    } else {
+      alert("Please Enter a valid number")
+      return false
+    }
+  }
+  var 
+})
